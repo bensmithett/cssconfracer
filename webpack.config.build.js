f279cdf97b6b2d505e10743a61c46d1d6db2bddf
Ben Smithett <ben@bensmithett.com>
2015-03-24 19:22:46 +1100

    Webpack build config

---
+++ webpack.config.build.js
@@ -0,0 +1,63 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  cache: true,
+  context: __dirname,
+  entry: {
+    index: [
+      "./src/index"
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./build"),
+    publicPath: "/build/"
+  },
+  resolve: {
+    extensions: ["", ".js", ".css", ".sass", ".jpg", ".png", ".svg", ".gif"],
+    modulesDirectories: ["src", "node_modules"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"]
+      },
+      {
+        test: /\.sass$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "autoprefixer-loader?browsers=last 2 version",
+          "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src")
+        ],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "autoprefixer-loader?browsers=last 2 version",
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|eot|ttf|woff)$/,
+        loader: "url-loader",
+        query: {
+          name: "[path][name].[ext]",
+          context: "src",
+          limit: "8192",
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    }),
+  ]
+};
